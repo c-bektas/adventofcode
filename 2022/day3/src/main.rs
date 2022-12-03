c92ab741cb475b3d1b14d99bd58102667ec6cb34
Caner Bektas <caner.bektas@tu-dortmund.de>
2022-12-03 12:54:12 +0100

    Day 3 Full Complete

--- 2022/day3/src/main.rs
+++ 2022/day3/src/main.rs
@@ -34,21 +34,25 @@ struct ElfGroup {
 }
 
 impl ElfGroup {
-    fn new(group: Vec<String>) -> ElfGroup {
+    fn new(group: &[&str]) -> ElfGroup {
         if group.len() != 3 {
             panic!("Should be 3");
         }
         else {
-            return ElfGroup{ common: 'a', priority: 1};
+            let common: char = find_common_char(group[0].to_string(), group[1].to_string(), group[2].to_string()).unwrap();
+            let priority = get_priority(common);
+
+            return ElfGroup{ common: common.to_owned(), priority: priority.to_owned()};
         }
     }
 
 }
 
+
 fn main() {
 
-    // TODO: Split in groups of three and input in new ElfGroups
-    let elf_groups = include_str!("../input_day3").lines();
+    let lines: Vec<&str> = include_str!("../input_day3").lines().collect();
+    let elf_groups = lines.chunks(3).map(|x| ElfGroup::new(x));
 
     let sum_prio: i32 = elf_groups.map(|x| x.priority).sum();
     println!("Sum Priority: {}", sum_prio);
