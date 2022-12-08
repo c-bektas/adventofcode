8906bf802d28ba09ca2584366d018b1f5949d421
Caner Bektas <caner.bektas@tu-dortmund.de>
2022-12-08 12:26:23 +0100

    Improvements to Day 8

--- 2022/day8/src/main.rs
+++ 2022/day8/src/main.rs
@@ -1,22 +1,9 @@
-fn calc_score(tree: &usize, neighbors: &[usize], reverse: bool) -> i32 {
+fn calc_score(tree: &usize, neighbors: &Vec<usize>) -> i32 {
     let mut score = 0;
-    if !reverse {
-        for (i, neighbor) in neighbors.iter().enumerate() {
-            if neighbor >= tree || i == neighbors.len()-1 {
-                score = i + 1;
-                break;
-            }
-        }
-
-    }
-
-    else {
-
-        for (i, neighbor) in neighbors.iter().rev().enumerate() {
-            if neighbor >= tree  || i == neighbors.len()-1 {
-                score = i + 1;
-                break;
-            }
+    for (i, neighbor) in neighbors.iter().enumerate() {
+        if neighbor >= tree || i == neighbors.len()-1 {
+            score = i + 1;
+            break;
         }
     }
     return score as i32;
@@ -38,13 +25,14 @@ fn main() {
     for (row_idx, row) in rows.iter().enumerate() {
         for (tree_idx, tree) in row.iter().enumerate() {
 
-            let top = &rows[0..row_idx].iter().map(|x| x[tree_idx]).collect::<Vec<usize>>();
+            // Reverse top and left slices for right point of view for tree
+            let top = &rows[0..row_idx].iter().map(|x| x[tree_idx]).rev().collect::<Vec<usize>>();
             let bottom = &rows[row_idx+1..rows.len()].iter().map(|x| x[tree_idx]).collect::<Vec<usize>>();
 
-            let left = &row[0..tree_idx];
-            let right = &row[tree_idx+1..row.len()];
+            let left = &row[0..tree_idx].to_vec().iter().rev().map(|x| x.to_owned()).collect::<Vec<usize>>();
+            let right = &row[tree_idx+1..row.len()].to_vec();
            
-            let score = calc_score(tree, top, true) * calc_score(tree, bottom, false) * calc_score(tree, left, true) * calc_score(tree, right, false);
+            let score = calc_score(tree, top) * calc_score(tree, bottom) * calc_score(tree, left) * calc_score(tree, right);
             if score > highest_score {
                 highest_score = score;
             }
